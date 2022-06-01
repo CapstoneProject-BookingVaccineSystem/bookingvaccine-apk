faeb370ed7e80c75cfe8e591a50d1280bfb3c3eb
Hayolox <akiklii72@gmail.com>
2022-06-01 20:07:36 +0700

    slicing sigin

---
+++ lib/screen/auth/sigin_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SiginScreen extends StatelessWidget {
+  const SiginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+}
