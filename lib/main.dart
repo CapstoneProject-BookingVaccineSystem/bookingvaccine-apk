e8f331056cc6b95f170940c29076141eeb177648
Hayolox <akiklii72@gmail.com>
2022-06-01 16:54:38 +0700

    slicing plash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import 'package:bookingvaccine/screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {}
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const SplashScreen(),
+      },
+    );
   }
 }
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,23 @@
+import 'package:bookingvaccine/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      body: SafeArea(
+        child: Center(
+          child: SvgPicture.asset(
+            'assets/logo.svg',
+            width: 342,
+            height: 342,
+          ),
+        ),
+      ),
+    );
+  }
+}
