e0008899647e0defd4be5742ea3dcfe98e697120
Hayolox <akiklii72@gmail.com>
2022-06-01 16:05:32 +0700

    preparation theme

---
+++ lib/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color backgroundColor1 = const Color(0xff7BD9E8);
+Color backgroundColor2 = const Color(0xffFFFFFF);
+
+Color primaryTextColor = const Color(0xff829CCC);
+Color secondTextColor = const Color(0xff4E7EA7);
+Color whiteTextColor = const Color(0xffFFFFFF);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  color: primaryTextColor,
+);
+
+TextStyle secondTextStyle = GoogleFonts.poppins(
+  color: secondTextColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteTextColor,
+);
