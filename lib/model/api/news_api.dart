1785a83686728cad151cc99557b5d7fc997361b5
Hayolox <akiklii72@gmail.com>
2022-07-08 19:23:23 +0700

    add token barier

--- lib/model/api/news_api.dart
+++ lib/model/api/news_api.dart
@@ -1,4 +1,5 @@
 import 'package:bookingvaccine/model/news_model/news_model.dart';
+import 'package:bookingvaccine/screen/storage/storage.dart';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -7,7 +8,13 @@ class NewsApi {
 
   Future getAllDataNews() async {
     try {
-      final _response = await Dio().get(_baseUrl + 'desc');
+      final String _token = await Storage().getToken();
+      final _response = await Dio().get(_baseUrl + 'desc',
+          options: Options(
+            headers: {
+              "authorization": "Bearer $_token",
+            },
+          ));
 
       List<dynamic> _getAllDataNews = _response.data;
       List<NewsModel> _allDataNews = [];
@@ -24,14 +31,12 @@ class NewsApi {
 
   Future getFiveDataNews() async {
     try {
-      final prefs = await SharedPreferences.getInstance();
-
-      final String? _token = prefs.getString('token');
+      final String _token = await Storage().getToken();
 
       final _response = await Dio().get(_baseUrl + '0/5',
           options: Options(
             headers: {
-              "authorization": "Bearer ${_token!}",
+              "authorization": "Bearer $_token",
             },
           ));
 
@@ -49,7 +54,13 @@ class NewsApi {
 
   getDetailNewsById(int id) async {
     try {
-      final _response = await Dio().get(_baseUrl + id.toString());
+      final String _token = await Storage().getToken();
+      final _response = await Dio().get(_baseUrl + id.toString(),
+          options: Options(
+            headers: {
+              "authorization": "Bearer $_token",
+            },
+          ));
       NewsModel dataNews = NewsModel.fromJson(_response.data['data']);
       return dataNews;
     } catch (e) {
